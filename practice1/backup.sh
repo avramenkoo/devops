a1f0b84eab8a0e7b23ffd336dbc4c67a06cb0d3f
Alexander <114441821+avramenkoo@users.noreply.github.com>
2023-04-30 20:48:54 +0000

    feat: exclude files

--- practice1/backup.sh
+++ practice1/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar -cvf ~/backups/backup-$(date +%Y-%m-%d).tar ~/some/important/directory
+tar -cvf ~/backups/backup-$(date +%Y-%m-%d).tar --exclude-from="exclude.txt" ~/some/important/directory
